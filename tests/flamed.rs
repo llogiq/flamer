010c0de7fd6157a4b8100433136dbebe6b7a0504
Andre Bogus <bogusandre@gmail.com>
2016-05-19 22:12:01 +0200

    actually test frame construction

--- tests/flamed.rs
+++ tests/flamed.rs
@@ -4,10 +4,8 @@
 
 extern crate flame;
 
-use std::fs::File;
-
 fn e() -> u32 {
-    1
+    2
 }
 
 fn d() -> u32 {
@@ -24,7 +22,7 @@ fn b() -> u32 {
 
 fn a() -> u32 {
     let mut result = 0;
-    for _ in 0..20 {
+    for _ in 0..3 {
         result += b()
     }
     result / 10
@@ -33,7 +31,9 @@ fn a() -> u32 {
 
 #[test]
 fn test_flame() {
-    assert_eq!(42, a());
-    
-    flame::dump_svg(&mut File::create("out.svg").unwrap()).unwrap();
+    assert_eq!(459, a());
+    let frames = flame::frames();
+    assert_eq!(1, frames.len());
+    let roots = &frames[0].roots;
+    assert_eq!(95, roots.len());
 }
